b47e5dbbb3b87d21fdbec37fbc720be523725901
JihanePett <jihane.petticrew@gmail.com>
2020-07-22 16:58:01 +0000

    modified the script.js to simplify the code by using axios get

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,16 @@
 const urls = [];
 
-window.addEventListener("DOMContentLoaded", () => {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://ghibliapi.herokuapp.com/films/");
-  xhr.send();
+window.addEventListener("DOMContentLoaded", async () => {
+  const movieList = await axios.get("https://ghibliapi.herokuapp.com/films/");
 
-  xhr.onload = function () {
-    var data = JSON.parse(this.response);
-
-    if (this.readyState == 4 && this.status == 200) {
-      var linkout = "";
-      var i;
-      for (i = 0; i < data.length; i++) {
-        linkout +=
-          `<a class="film-url" href="javascript:populatemodal('${
-            data[i].url
-          }', '${data[i].title.replace(/'/gi, "")}')">` +
-          data[i].title +
-          "</a><br>";
-      }
-
-      document.getElementById("placeholder").innerHTML = linkout;
-    }
-  };
+  movieList.data.forEach((movie) => {
+    document.getElementById("placeholder").innerHTML +=
+      `<a class="film-url" href="javascript:populatemodal
+        ('${movie.url}', '${movie.title.replace(/'/gi, "")}')">` +
+      movie.title +
+      "</a><br>";
+  });
 });
-
-const getdata = async function () {
-  const arrayOfPromises = urls.map((url) => axios.get(url[0]));
-  for await (let request of arrayOfPromises) {
-    $("#modalBody").text(JSON.stringify(request.data));
-  }
-};
 
 async function populatemodal(url, title) {
   // set the modal title
@@ -39,19 +19,28 @@ async function populatemodal(url, title) {
   // Get the data from the selected URL
   const response = await axios.get(url);
 
-  await buildView(response.data.people, 'People');
-  await buildView(response.data.species, 'Species');
-  await buildView(response.data.locations, 'Locations');
-  await buildView(response.data.vehicles, 'Vehicles');
+  await buildView(response.data.people, "People");
+  await buildView(response.data.species, "Species");
+  await buildView(response.data.locations, "Locations");
+  await buildView(response.data.vehicles, "Vehicles");
 
   // Show Modalspecies
   $("#staticBackdrop").modal("toggle");
 }
 
 async function buildView(array_data, header) {
+  // Loop through the Urls and get an array of response
   const arrayOfPromises = array_data.map((url) => axios.get(url));
+
+  // Loop through the response from the URL
   for await (let response of arrayOfPromises) {
     // $("#modalBody").text(JSON.stringify(response.data));
-    $(`<h3>${header}</h3><p>${JSON.stringify(response.data).replace(/,/g, '<br>')}</p>`).appendTo('#modalBody');
+    // Write out the header and the json string of the response.
+    $(
+      `<h3>${header}</h3><p>${JSON.stringify(response.data).replace(
+        /,/g,
+        "<br>"
+      )}</p>`
+    ).appendTo("#modalBody");
   }
 }
