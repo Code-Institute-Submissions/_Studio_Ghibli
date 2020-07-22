d62d8677f131f6f9ec3b6b20625138a711f5f313
JihanePett <jihane.petticrew@gmail.com>
2020-07-22 15:56:16 +0000

    added script to films.js to populate the description of each movie in the corresponding back flip cards. deleted card-columns and opted for card-deck to display the flip cards in films.html and updated the container fluid and cards style, replaced the howlsmovingcastle image by a smaller one in the images folder and amended the html links to the about and contact pages in all .html pages

--- assets/js/films.js
+++ assets/js/films.js
@@ -1,19 +1,309 @@
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
+xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description").innerHTML = myObj.description;
+    document.getElementById("date").innerHTML = myObj.release_date;
+    document.getElementById("rt-score").innerHTML = myObj.rt_score;
   }
 };
-xmlhttp.open("GET", "https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe/", true);
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/2baf70d1-42bb-4437-b551-e5fed5a87abe/",
+  true
+);
 xmlhttp.send();
 
 var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function() {
+xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description1").innerHTML = myObj.description;
   }
 };
-xmlhttp.open("GET", "https://ghibliapi.herokuapp.com/films/45db04e4-304a-4933-9823-33f389e8d74d/", true);
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/45db04e4-304a-4933-9823-33f389e8d74d/",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description2").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/cd3d059c-09f4-4ff3-8d63-bc765a5184fa",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description3").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description3").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description3").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description4").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/45204234-adfd-45cb-a505-a8e7a676b114",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description5").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/58611129-2dbc-4a81-a72f-77ddfc1b1b49",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description6").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description7").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/4e236f34-b981-41c3-8c65-f8c9000b94e7",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description8").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/1b67aa9a-2e4a-45af-ac98-64d6ad15b16c",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description9").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/758bf02e-3122-46e0-884e-67cf83df1786",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description10").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/ebbb6b7c-945c-41ee-a792-de0e43191bd8",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description11").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/0440483e-ca0e-4120-8c50-4c8cd9b965d6",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description12").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/dc2e6bd1-8156-4886-adff-b39e6043af0c",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description13").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description14").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description15").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description16").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description17").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/5fdfb320-2a02-49a7-94ff-5ca418cae602",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description18").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/ff24da26-a969-4f0e-ba1e-a122ead6c6e3",
+  true
+);
+xmlhttp.send();
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("description19").innerHTML = myObj.description;
+  }
+};
+xmlhttp.open(
+  "GET",
+  "https://ghibliapi.herokuapp.com/films/67405111-37a5-438f-81cc-4666af60c800",
+  true
+);
 xmlhttp.send();
