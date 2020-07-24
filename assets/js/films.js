94b3c93106251b5a494ae1dab839367678ee9434
JihanePett <jihane.petticrew@gmail.com>
2020-07-24 10:17:17 +0000

    adding more data from the api into films.html through films.js, updated the flip card style in style.css

--- assets/js/films.js
+++ assets/js/films.js
@@ -2,9 +2,11 @@ var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
-    document.getElementById("description").innerHTML = myObj.description;
-    document.getElementById("date").innerHTML = myObj.release_date;
-    document.getElementById("rt-score").innerHTML = myObj.rt_score;
+    document.getElementById("description").innerHTML = ` ${myObj.description}...`;
+    document.getElementById("date").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -19,6 +21,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description1").innerHTML = myObj.description;
+        document.getElementById("date1").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score1").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director1").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer1").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -33,8 +39,13 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description2").innerHTML = myObj.description;
+        document.getElementById("date2").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score2").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director2").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer2").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
+
 xmlhttp.open(
   "GET",
   "https://ghibliapi.herokuapp.com/films/cd3d059c-09f4-4ff3-8d63-bc765a5184fa",
@@ -47,34 +58,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description3").innerHTML = myObj.description;
-  }
-};
-xmlhttp.open(
-  "GET",
-  "https://ghibliapi.herokuapp.com/films/ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
-  true
-);
-xmlhttp.send();
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    document.getElementById("description3").innerHTML = myObj.description;
-  }
-};
-xmlhttp.open(
-  "GET",
-  "https://ghibliapi.herokuapp.com/films/ea660b10-85c4-4ae3-8a5f-41cea3648e3e",
-  true
-);
-xmlhttp.send();
-
-var xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
-  if (this.readyState == 4 && this.status == 200) {
-    var myObj = JSON.parse(this.responseText);
-    document.getElementById("description3").innerHTML = myObj.description;
+        document.getElementById("date3").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score3").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director3").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer3").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -89,6 +76,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description4").innerHTML = myObj.description;
+        document.getElementById("date4").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score4").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director4").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer4").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -103,6 +94,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description5").innerHTML = myObj.description;
+        document.getElementById("date5").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score5").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director5").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer5").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -117,6 +112,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description7").innerHTML = myObj.description;
+        document.getElementById("date7").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score7").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director7").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer7").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -131,6 +130,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description8").innerHTML = myObj.description;
+        document.getElementById("date8").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score8").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director8").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer8").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -145,6 +148,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description9").innerHTML = myObj.description;
+        document.getElementById("date9").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score9").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director9").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer9").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -159,6 +166,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description10").innerHTML = myObj.description;
+        document.getElementById("date10").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score10").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director10").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer10").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -173,6 +184,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description11").innerHTML = myObj.description;
+        document.getElementById("date11").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score11").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director11").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer11").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -187,6 +202,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description12").innerHTML = myObj.description;
+        document.getElementById("date12").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score12").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director12").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer12").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -201,6 +220,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description17").innerHTML = myObj.description;
+        document.getElementById("date17").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score17").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director17").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer17").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -215,6 +238,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description18").innerHTML = myObj.description;
+        document.getElementById("date18").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score18").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director18").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer18").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
@@ -229,6 +256,10 @@ xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     var myObj = JSON.parse(this.responseText);
     document.getElementById("description19").innerHTML = myObj.description;
+        document.getElementById("date19").innerHTML = `Release date: ${myObj.release_date}`;
+    document.getElementById("rt-score19").innerHTML = `Rt score: ${myObj.rt_score} %`;
+    document.getElementById("director19").innerHTML = `Director: ${myObj.director}`;
+    document.getElementById("producer19").innerHTML = `Producer: ${myObj.producer}`;
   }
 };
 xmlhttp.open(
