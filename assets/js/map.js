5c1afea7f6fe26e4d1d8a22cc1fc044d6ee3e3c9
JihanePett <jihane.petticrew@gmail.com>
2020-07-25 11:33:19 +0000

    restricted the google maps api to my page and updated the coordinates on map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,7 @@
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 6.2603, lng: 53.3498 },
+    center: { lat: 53.3498, lng: 6.2603 },
     zoom: 4
   });
+  
 }
